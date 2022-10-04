d9021274cacd92ca2f279b68ffa80fc2f612164a
Jon Massey <jon.massey@thedatalab.org>
2022-10-04 15:58:51 +0100

    measures config

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,4 +1,4 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import StudyDefinition, patients, Measure
 from demographic_variables import demographic_variables
 
 
@@ -12,19 +12,29 @@ study = StudyDefinition(
     population=patients.satisfying(
         "complete_record AND registered",
         complete_record=patients.with_complete_gp_consultation_history_between(
-            "2021-01-01", "2021-12-31"),
+            "2021-01-01", "2021-12-31"
+        ),
         registered=patients.registered_with_one_practice_between(
-            "2021-01-01", "2021-12-31")
+            "2021-01-01", "2021-12-31"
+        ),
     ),
     gp_consultation_count=patients.with_gp_consultations(
         between=["index_date", "index_date + 1 month"],
         returning="number_of_matches_in_period",
         return_expectations={
             "int": {"distribution": "poisson", "mean": 3},
-            "incidence": 0.25
-            }
-        ),
-    **demographic_variables
-    )
+            "incidence": 0.25,
+        },
+    ),
+    **demographic_variables,
+)
 
-# MEASURES!?
+measures = [
+    Measure(
+        id=f"gp_consultations_by_{d}",
+        numerator="gp_consultation_count",
+        denominator="population",
+        group_by=d,
+    )
+    for d in demographic_variables.keys()
+]
